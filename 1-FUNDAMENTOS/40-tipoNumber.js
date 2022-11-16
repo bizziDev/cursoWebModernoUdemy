97eb58619ec11413b474038e9fa879714f943aa0
mari <mari@pop-os.localdomain>
2022-11-16 14:50:33 -0300

    fundamentos

---
+++ 1-FUNDAMENTOS/40-tipoNumber.js
@@ -0,0 +1,18 @@
+{ 
+  const peso1 = 2;
+  const peso2 = "2";
+  console.log(peso1 + peso2);
+  console.log(typeof peso2);
+  {
+    const n1 = 4;
+    const n2 = 4;
+    console.log(n1 + n2);
+    console.log(typeof n1, typeof n2);
+  }
+}
+
+//string são delimitadas por aspas duplas (""), aspas simples('') ou crase (``)
+
+//blocos de codigo funcionam como um paragrafo comparado com um livro, e sentençã uma frase 
+//blocos delimitado por ({})
+//sentença com (;) no final
